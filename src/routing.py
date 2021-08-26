488127fc680329f767d268fd17df3f8c3b11d895
juan <juansun@systex.com>
2021-08-26 11:16:38 +0800

    [dev] channel asgi settings

---
+++ src/routing.py
@@ -0,0 +1,15 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from chat.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    'websocket': AllowedHostsOriginValidator(  # 繼承settings.ALLOWED_HOSTS
+        AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        ),
+    )
+})
