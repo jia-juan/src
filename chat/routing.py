4a9577b73851916573531d144a60cac0a4514918
juan <juansun@systex.com>
2021-08-28 02:16:54 +0800

    [tut] channels documents chat tutorial

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from chat.consumers import EchoConsumer
 
+from . import consumers
+
 websocket_urlpatterns = [
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+
     # nginx locate at uri /ws/...
     path('ws/echo/', EchoConsumer.as_asgi()),  # 記得.as_asgi() memo 210816 ISSUE: TypeError: XXX() has no arguments???
     # path('ws/<str:username>/', MessageConsumer.as_asgi())  # self.scope['url_route']['kwargs']['username']
--- src/urls.py
+++ src/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from prof.views import (
     home_screen_view
 )
 
 urlpatterns = [
-    path('', home_screen_view, name='home'),
+    path('chat/', include('chat.urls')),
+    # path('prof/', home_screen_view, name='home'),
     path('admin/', admin.site.urls),
 ]
