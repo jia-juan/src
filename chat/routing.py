0ae8ea373a0ecdf7da7a8f5a37ef7bc053d7b311
juan <juansun@systex.com>
2021-08-27 16:58:44 +0800

    [fix] route should match to nginx location route pattern

--- chat/routing.py
+++ chat/routing.py
@@ -2,6 +2,7 @@ from django.urls import path
 from chat.consumers import EchoConsumer
 
 websocket_urlpatterns = [
-    path('echo/', EchoConsumer.as_asgi()),  # 記得.as_asgi() memo 210816 ISSUE: TypeError: XXX() has no arguments???
+    # nginx locate at uri /ws/...
+    path('ws/echo/', EchoConsumer.as_asgi()),  # 記得.as_asgi() memo 210816 ISSUE: TypeError: XXX() has no arguments???
     # path('ws/<str:username>/', MessageConsumer.as_asgi())  # self.scope['url_route']['kwargs']['username']
 ]
