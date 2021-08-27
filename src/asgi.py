e78072cb1f2ffc6cdce8422940b76dd703823399
juan <juansun@systex.com>
2021-08-27 14:12:57 +0800

    [dev] asgi redesign for environment variable

--- src/asgi.py
+++ src/asgi.py
@@ -1,7 +1,24 @@
 import os
-import django
-import channels.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
-django.setup()
-application = channels.routing.get_default_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
+# from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+# django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    # "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            url(r"^chat/admin/$", AdminChatConsumer.as_asgi()),
+            url(r"^chat/$", PublicChatConsumer.as_asgi()),
+        ])
+    ),
+})
