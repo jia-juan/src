791b50b69da9674a8ed40488cbdb4051c4566f62
juan <juansun@systex.com>
2021-08-28 00:50:06 +0800

    [dev] static/media settings

--- src/settings.py
+++ src/settings.py
@@ -139,12 +139,16 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collect/')  # python manage.py collectstatic
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'media')
+]
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'), ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_collect/')
 
 
 # Channel layers
