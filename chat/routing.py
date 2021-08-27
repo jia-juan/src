62e5604ba4bae25a807aeeb25030f81421251d88
juan <jiajuansun@gmail.com>
2021-08-27 09:24:00 +0000

    deploy successfully

--- chat/routing.py
+++ chat/routing.py
@@ -2,6 +2,6 @@ from django.urls import path
 from chat.consumers import EchoConsumer
 
 websocket_urlpatterns = [
-    path('echo/', EchoConsumer.as_asgi()),  # 記得.as_asgi() memo 210816 ISSUE: TypeError: XXX() has no arguments???
+    path('ws/echo/', EchoConsumer.as_asgi()),  # 記得.as_asgi() memo 210816 ISSUE: TypeError: XXX() has no arguments???
     # path('ws/<str:username>/', MessageConsumer.as_asgi())  # self.scope['url_route']['kwargs']['username']
 ]
