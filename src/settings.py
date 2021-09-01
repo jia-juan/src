2caa6da98adb23f8c99cc9a46bc87ef0526796af
jiajuan <jia-juan@users.noreply.github.com>
2021-09-01 20:57:25 +0800

    [fix] channels layer using in-memory

--- src/settings.py
+++ src/settings.py
@@ -158,8 +158,8 @@ MEDIA_URL = '/media/'
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],  # todo [dev]
-        },
+#        'CONFIG': {
+#            "hosts": [('127.0.0.1', 6379)],  # todo [dev]
+        # },
     },
 }
